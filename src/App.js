d755208ce287287d3abadf30c8266ebd3ebdc695
Dejan <dejandavidovic1989@gmail.com>
2018-04-04 13:08:44 +0200

    Adding new components

--- src/App.js
+++ src/App.js
@@ -6,16 +6,63 @@ let blue = 'blue'
 let black = 'black'
 let white = 'white'
 let margin = '10px'
-class Navbar extends Component{
+let history = {user:
+  {lessons: 'Ancient Grecce',
+   
+  }
+};
+class History extends Component{
+   
     render() {
       return(
-          <div style={{height:'50px', background:black,  color:white, width:'30%', display:'inline-block',margin}}>
-             <h2 style={{margin}}> Button  </h2>
+          <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
+             <h2 style={{margin}}> History </h2>
              
           </div>
       ); 
     }
 }
+
+class Geography extends Component{
+   
+  render() {
+    return(
+        <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
+           <h2 style={{margin}}> Geography </h2>
+           
+        </div>
+    ); 
+  }
+}
+
+
+class Politics extends Component{
+   
+  render() {
+    return(
+        <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
+           <h2 style={{margin}}> Politics </h2>
+           
+        </div>
+    ); 
+  }
+}
+
+
+
+class Culture extends Component{
+   
+  render() {
+    return(
+        <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
+           <h2 style={{margin}}> Culture </h2>
+           
+        </div>
+    ); 
+  }
+}
+
+
 
 
 class Main extends Component {
@@ -41,19 +88,37 @@ class Footer extends Component {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+
+  componentDidMount(){
+    setTimeout(() =>{
+    this.setState({serverData: history});
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1>Title </h1>
-        <Navbar/>
-        <Navbar/>
-        <Navbar/>
+        {this.state.serverData.user ?
+        <div>
+         <h1> 
+          {this.state.serverData.user.lessons} lessons
+        </h1>
+        <History />
+        <Geography />
+        <Politics />
+        <Culture/>
+        
         <Main/>
-        <Footer/>
+        <Footer/></div>: <h1>Loading...</h1>
+        }
         
         
-      </div>
+        </div> 
     );
+    
   }
 }
 
