929ef437767405d8a5b47bf12e85db28f9877192
Dejan <dejandavidovic1989@gmail.com>
2018-04-05 12:57:06 +0200

    Adding changes to our components

--- src/App.js
+++ src/App.js
@@ -6,70 +6,61 @@ let blue = 'blue'
 let black = 'black'
 let white = 'white'
 let margin = '10px'
-let history = {user:
-  {lessons: 'Ancient Grecce',
+let player = {user:
+  {type: 'MP3 music player',
+  playlists:[
+    {name:'My List',
+     songs:[{name: 'song 1', duration:189},{ name:'song 2', duration:204},{name: 'song 3', duration:198}]},
+    {name:'My List 1',
+     songs:[{name: 'song 1.1', duration:198},{ name:'song 1.2', duration:214},{name: 'song 1.3', duration:215}]},
+    {name:'My List 2',
+     songs:[{name: 'song 2.1', duration:300},{ name:'song 2.2', duration:224},{name: 'song 2.3', duration:298}]},
+    {name:'My List 3',
+     songs:[{name: 'song 3.1', duration:199},{ name:'song 3.2', duration:209},{name: 'song 3.3', duration:187}]},
+  ]
+         
    
   }
 };
-class History extends Component{
+
+class Playlists extends Component{
    
-    render() {
+    render() {  
       return(
-          <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
-             <h2 style={{margin}}> History </h2>
+          <div style={{height:'50px', background:black,  color:white, width:'40%', display:'inline-block',margin}}>
+             <h2 style={{margin}}>{this.props.playlists.length} Playlists </h2>
              
           </div>
       ); 
     }
 }
 
-class Geography extends Component{
-   
-  render() {
-    return(
-        <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
-           <h2 style={{margin}}> Geography </h2>
-           
-        </div>
-    ); 
+class Filter extends Component {
+  render (){
+    return (
+      <div style={{ background:black,  color:white, width:'40%', display:'inline-block'}}>
+        <input type='text' onKeyUp={event=>
+          this.props.onTextChange(event.target.value)}/>
+         
+      </div>);
+    }
   }
-}
 
-
-class Politics extends Component{
-   
-  render() {
-    return(
-        <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
-           <h2 style={{margin}}> Politics </h2>
-           
-        </div>
-    ); 
-  }
-}
-
-
-
-class Culture extends Component{
-   
-  render() {
-    return(
-        <div style={{height:'50px', background:black,  color:white, width:'20%', display:'inline-block',margin}}>
-           <h2 style={{margin}}> Culture </h2>
-           
-        </div>
-    ); 
-  }
-}
 
 
 
 
 class Main extends Component {
   render(){
+    let playlists= this.props.playlists
     return(
-      <div style={{background:black, color:white, margin, height:'500px'}}>
-        Main Bar
+      <div id='list'>
+        <h3>{playlists.name}</h3>
+        <ul>
+          {playlists.songs.map(song=>
+              <li>{song.name }  -   {song.duration} seconds</li>
+          )}
+        </ul>
       </div>
     );
   }   
@@ -90,13 +81,15 @@ class Footer extends Component {
 class App extends Component {
   constructor() {
     super();
-    this.state = {serverData: {}}
+    this.state = {serverData: {},
+                  filterString:''}
   }
 
   componentDidMount(){
     setTimeout(() =>{
-    this.setState({serverData: history});
+    this.setState({serverData: player});
     }, 1000);
+    
   }
   render() {
     return (
@@ -104,17 +97,14 @@ class App extends Component {
         {this.state.serverData.user ?
         <div>
          <h1> 
-          {this.state.serverData.user.lessons} lessons
+          {this.state.serverData.user.type} 
         </h1>
-        <History />
-        <Geography />
-        <Politics />
-        <Culture/>
-        
-        <Main/>
-        <Footer/></div>: <h1>Loading...</h1>
-        }
-        
+        <Playlists playlists={this.state.serverData.user.playlists} />
+        <Filter onTextChange={text => this.setState({filterString: text})}/>
+        {this.state.serverData.user.playlists.filter(playlists=>playlists.name.toLowerCase().includes(this.state.filterString.toLowerCase()))
+        .map(playlists=>
+        <Main  playlists={playlists}/>)}
+        <Footer/></div>: <h1>Loading...</h1>}
         
         </div> 
     );
@@ -123,3 +113,4 @@ class App extends Component {
 }
 
 export default App;
+
